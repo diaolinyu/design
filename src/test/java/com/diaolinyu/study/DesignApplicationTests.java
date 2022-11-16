7b12fc0836951643b6a61a8b7e4c5b4f6185eb9f
dly <1767662413@qq.com>
2022-11-16 16:15:16 +0800

    init design

---
+++ src/test/java/com/diaolinyu/study/DesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diaolinyu.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
